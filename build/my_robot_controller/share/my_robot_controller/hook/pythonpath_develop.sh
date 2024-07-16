b6a446386ae2a2da5c93e602da2798d36d0a7c55
krisha <158523377+krishamangrola@users.noreply.github.com>
2024-07-15 23:04:41 -0400

    Add files via upload

--- build/my_robot_controller/share/my_robot_controller/hook/pythonpath_develop.sh
+++ build/my_robot_controller/share/my_robot_controller/hook/pythonpath_develop.sh
@@ -1,3 +1,3 @@
 # generated from colcon_core/shell/template/hook_prepend_value.sh.em
 
-_colcon_prepend_unique_value PYTHONPATH "/home/krisha/ros2_ws/src/build/my_robot_controller"
+_colcon_prepend_unique_value PYTHONPATH "/home/krisha/ros2_ws/build/my_robot_controller"
